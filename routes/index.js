61dbd501dcb716da0d97e373bde5908b0b024280
Ueslei de Souza <udsndesign@gmail.com>
2016-01-09 12:41:00 -0200

    Delete index.js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var index = function (req, res, next) {
-	res.render('index', { 
-		title: 'Hey, this is index page.', 
-		message: 'Hello there, you are now on index page.'
-	});
-}
-
-module.exports = index;
